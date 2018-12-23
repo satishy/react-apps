22e634dc2f08f201b970741936b7861c441e378c
Satish Yeruva <research.yeruva@gmail.com>
2018-12-23 11:15:38 +0000

    initial commit

---
+++ react-auth/server/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const keys = require('./config/keys');
+const cookieSession = require('cookie-session');
+require('./models/Users');
+require('./services/passport');
+
+mongoose.connect(keys.mongoURI);
+
+const app = express();
+
+app.use(
+  cookieSession({
+      maxAge: 30 * 24 * 60 * 60 * 1000,
+      keys: [keys.cookieKey]
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authRoutes')(app);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
